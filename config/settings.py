72511c9c12a0f1f998771c3d640ff4f3a35f3636
Maksat <mkartanbaev93@gmail.com>
2023-04-25 12:05:56 +0600

    register and login user

--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,10 @@ SWAGGER_SETTINGS = {
 
 AUTH_USER_MODEL = 'account.User'
 
+from datetime import timedelta
+
+SIMPLE_JWT ={
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
